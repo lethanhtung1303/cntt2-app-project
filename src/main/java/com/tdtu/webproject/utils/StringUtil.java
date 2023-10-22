9a3dfc3f19aac837577b4fa5a82ce12d6bd84fe1
lethanhtung1303 <letung844@gmail.com>
2023-10-22 22:47:58 +0700

    add web & controller

---
+++ src/main/java/com/tdtu/webproject/utils/StringUtil.java
@@ -0,0 +1,15 @@
+package com.tdtu.webproject.utils;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+public class StringUtil {
+    public static String convertBigDecimalToString(BigDecimal number) {
+        return Optional.ofNullable(number).isPresent() ?
+                String.valueOf(number) : null;
+    }
+
+    public static boolean isNotNullOrEmptyString(String string) {
+        return Optional.ofNullable(string).isPresent() && !string.isEmpty() && !string.isBlank();
+    }
+}
