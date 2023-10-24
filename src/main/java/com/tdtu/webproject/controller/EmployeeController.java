b93e50dc26e7d01c51f94cbe0e0607218c919f74
lethanhtung1303 <letung844@gmail.com>
2023-10-24 23:05:53 +0700

    add end of file

--- src/main/java/com/tdtu/webproject/controller/EmployeeController.java
+++ src/main/java/com/tdtu/webproject/controller/EmployeeController.java
@@ -4,11 +4,16 @@ import com.tdtu.webproject.service.EmployeeService;
 import generater.openapi.api.EmployeeApi;
 import generater.openapi.model.EmployeeResponse;
 import generater.openapi.model.EmployeeResponseResults;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.Pattern;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -18,7 +23,12 @@ public class EmployeeController implements EmployeeApi {
     private final EmployeeService employeeService;
 
     @Override
-    public ResponseEntity<EmployeeResponse> employee(String employeeIds) {
+    public ResponseEntity<EmployeeResponse> employee(
+            @Pattern(regexp = "^(\\d{1,19},)*\\d{1,19}$")
+            @ApiParam(value = "")
+            @Validated
+            @RequestParam(value = "employeeIds", required = false)
+            String employeeIds) {
         return ResponseEntity.ok(EmployeeResponse.builder()
                 .status(HttpStatus.OK.value())
                 .results(EmployeeResponseResults.builder()
